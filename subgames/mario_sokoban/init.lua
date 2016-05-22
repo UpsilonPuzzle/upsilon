6e11e01be9e7b8757b6c453e862625444736259c
paly2 <plangrognet@laposte.net>
2016-05-22 19:53:58 +0200

    Change mario_sokoban subgame

--- subgames/mario_sokoban/init.lua
+++ subgames/mario_sokoban/init.lua
@@ -16,16 +16,16 @@ upsilon.register_node("wall", {
 })
 
 -- Callbacks
-upsilon.register_on_down(function()
+local function move_mario(x, y)
 	local mario_pos = upsilon.get_node_pos("mario")[0]
-	local node = upsilon.get_node(mario_pos.x, mario_pos.y+1)
+	local node = upsilon.get_node(mario_pos.x+x, mario_pos.y+y)
 	if node == "wall" then
 		return
 	elseif node == "box" or node == "box_ok" then
-		if upsilon.get_node(mario_pos.x, mario_pos.y+2) == "none" then
-			upsilon.set_node(mario_pos.x, mario_pos.y+2, "box")
-		elseif upsilon.get_node(mario_pos.x, mario_pos.y+2) == "goal" then
-			upsilon.set_node(mario_pos.x, mario_pos.y+2, "box_ok")
+		if upsilon.get_node(mario_pos.x+x*2, mario_pos.y+y*2) == "none" then
+			upsilon.set_node(mario_pos.x+x*2, mario_pos.y+y*2, "box")
+		elseif upsilon.get_node(mario_pos.x+x*2, mario_pos.y+y*2) == "goal" then
+			upsilon.set_node(mario_pos.x+x*2, mario_pos.y+y*2, "box_ok")
 			if #upsilon.get_node_pos("goal") == 0 then
 				upsilon.next_map()
 			end
@@ -33,62 +33,18 @@ upsilon.register_on_down(function()
 			return
 		end
 	end
-	upsilon.move_node(mario_pos.x, mario_pos.y, 0, 1)
+	upsilon.move_node(mario_pos.x, mario_pos.y, x, y)
+end
+
+upsilon.register_on_down(function()
+	move_mario(0,1)
 end)
 upsilon.register_on_up(function()
-	local mario_pos = upsilon.get_node_pos("mario")[0]
-	local node = upsilon.get_node(mario_pos.x, mario_pos.y-1)
-	if node == "wall" then
-		return
-	elseif node == "box" or node == "box_ok" then
-		if upsilon.get_node(mario_pos.x, mario_pos.y-2) == "none" then
-			upsilon.set_node(mario_pos.x, mario_pos.y-2, "box")
-		elseif upsilon.get_node(mario_pos.x, mario_pos.y-2) == "goal" then
-			upsilon.set_node(mario_pos.x, mario_pos.y-2, "box_ok")
-			if #upsilon.get_node_pos("goal") == 0 then
-				upsilon.next_map()
-			end
-		else
-			return
-		end
-	end
-	upsilon.move_node(mario_pos.x, mario_pos.y, 0, -1)
+	move_mario(0,-1)
 end)
 upsilon.register_on_right(function()
-	local mario_pos = upsilon.get_node_pos("mario")[0]
-	local node = upsilon.get_node(mario_pos.x+1, mario_pos.y)
-	if node == "wall" then
-		return
-	elseif node == "box" or node == "box_ok" then
-		if upsilon.get_node(mario_pos.x+2, mario_pos.y) == "none" then
-			upsilon.set_node(mario_pos.x+2, mario_pos.y, "box")
-		elseif upsilon.get_node(mario_pos.x+2, mario_pos.y) == "goal" then
-			upsilon.set_node(mario_pos.x+2, mario_pos.y, "box_ok")
-			if #upsilon.get_node_pos("goal") == 0 then
-				upsilon.next_map()
-			end
-		else
-			return
-		end
-	end
-	upsilon.move_node(mario_pos.x, mario_pos.y, 1, 0)
+	move_mario(1,0)
 end)
 upsilon.register_on_left(function()
-	local mario_pos = upsilon.get_node_pos("mario")[0]
-	local node = upsilon.get_node(mario_pos.x-1, mario_pos.y)
-	if node == "wall" then
-		return
-	elseif node == "box" or node == "box_ok" then
-		if upsilon.get_node(mario_pos.x-2, mario_pos.y) == "none" then
-			upsilon.set_node(mario_pos.x-2, mario_pos.y, "box")
-		elseif upsilon.get_node(mario_pos.x-2, mario_pos.y) == "goal" then
-			upsilon.set_node(mario_pos.x-2, mario_pos.y, "box_ok")
-			if #upsilon.get_node_pos("goal") == 0 then
-				upsilon.next_map()
-			end
-		else
-			return
-		end
-	end
-	upsilon.move_node(mario_pos.x, mario_pos.y, -1, 0)
+	move_mario(-1,0)
 end)
